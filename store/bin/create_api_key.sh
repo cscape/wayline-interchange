ae9790ee92b441e5d2294a9c444c30e2a0061ea4
ciolt <cioltdev@outlook.com>
2019-05-18 19:47:52 -0400

    fix up api key gen

--- store/bin/create_api_key.sh
+++ store/bin/create_api_key.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 echo 'THETRANSITCLOCK DOCKER: Create API key for $TRANSITCLOCK_AGENCY_PROPERTIES_FILE'
 
-# Finds and replaces POSTGRES_PORT_5432_TCP_PORT + POSTGRES_PORT_5432_TCP_ADDR in properties file
-find $TRANSITCLOCK_AGENCY_PROPERTIES_FILE -type f -exec sed -i s#POSTGRES_PORT_5432_TCP_ADDR#$POSTGRES_PORT_5432_TCP_ADDR#g {} \;
-find $TRANSITCLOCK_AGENCY_PROPERTIES_FILE -type f -exec sed -i s#POSTGRES_PORT_5432_TCP_PORT#$POSTGRES_PORT_5432_TCP_PORT#g {} \;
-
 java -jar CreateAPIKey.jar \
-  -c $TRANSITCLOCK_AGENCY_PROPERTIES_FILE \
-  -d "Connector" \
-  -e "alex@example.com" \
+  -config $TRANSITCLOCK_AGENCY_PROPERTIES_FILE \
+  -description "Connector" \
+  -email "alex@example.com" \
   -name "Application" \
-  -p "123456" \
-  -u "https://wayline.co"
+  -phone "123456" \
+  -url "https://wayline.co"
