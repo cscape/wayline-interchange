8c21a6df9ad3c593786faa5efd4928167637e1c8
Alejandra <cioltdev@outlook.com>
2019-06-26 19:50:24 -0400

    Use less jars - create_webagency.sh

--- store/bin/create_webagency.sh
+++ store/bin/create_webagency.sh
@@ -9,13 +9,13 @@ set -u
 java \
   -Dtransitclock.db.dbName="TC_AGENCY_${AGENCYID}" \
   -Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
-  -Dtransitclock.db.dbHost=$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT \
+  -Dtransitclock.db.dbHost="${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}" \
   -Dtransitclock.db.dbUserName=$PGUSERNAME \
   -Dtransitclock.db.dbPassword=$PGPASSWORD \
   -Dtransitclock.db.dbType=postgresql \
   -Dhibernate.connection.username="${PGUSERNAME}" \
   -Dhibernate.connection.password="${PGPASSWORD}" \
-  -jar /usr/local/transitclock/CreateWebAgency.jar \
+  -cp /usr/local/transitclock/Core.jar org.transitclock.db.webstructs.WebAgency \
   "${AGENCYID}" \
   127.0.0.1 \
   "TC_AGENCY_${AGENCYID}" \
