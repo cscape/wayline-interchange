8828cb37ab1fd7c686d38acecff43a4ab9a125d1
Alejandra <cioltdev@outlook.com>
2019-06-26 15:02:01 -0400

    Update node-build.js

--- node-build.js
+++ node-build.js
@@ -7,26 +7,27 @@ const { doEverything, agencyIdFromNumber } = require('./lib/agency.properties')
 
 // Setting defaults,
 // PLEASE explicitly pass the -user, -pass, and -host parameters when calling this script!
-let dbUser = process.env.PGUSERNAME || 'TCUser'
-let dbPass = process.env.PGPASSWORD || 'sample_interchangePassword145'
-let dbHost = process.env.POSTGRES_PORT_5432_TCP_ADDR || '127.0.0.1'
+let dbUser = process.env.PGUSERNAME || 'ERROR_USERNAME_NOT_SET'
+let dbPass = process.env.PGPASSWORD || 'ERROR_PASSWORD_NOT_SET'
+let dbHost = process.env.POSTGRES_PORT_5432_TCP_ADDR || 'ERROR_HOST_NOT_SET'
 let noBuild = false
 
 process.argv.forEach(a => {
   if (a.indexOf('-') !== 0) return
   const key = String(a.split('=')[0].replace(/-/gm, '')).toLowerCase().trim()
   const val = String(a.split('=')[1]).trim()
-  switch (key) {
-    case 'user': dbUser = val; break;
-    case 'pass': dbPass = val; break;
-    case 'host': dbHost = val; break;
-    case 'nobuild': noBuild = Boolean(val); break;
-  }
 
   // Prevent silent failure
   if (['user', 'pass', 'host'].indexOf(key) === -1) {
     throw new Error('FATAL! You need to specify the DB Username, DB Password, and DB Host when calling this script.')
     process.exit(3)
+  }
+
+  switch (key) {
+    case 'user': dbUser = val; break;
+    case 'pass': dbPass = val; break;
+    case 'host': dbHost = val; break;
+    case 'nobuild': noBuild = Boolean(val); break;
   }
 })
 
@@ -46,3 +47,4 @@ const agencies = data.agency.map(agency => {
 }).join('\n')
 
 console.log(String(agencies))
+process.exit(0)
